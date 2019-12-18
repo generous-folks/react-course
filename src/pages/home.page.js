02ac6fb958484570a5fe93988bfefa502449964a
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2019-12-19 00:37:46 +0100

    feat(cart): add basic cart component and wrapper (#11)

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { Layout } from '../components/layout.component';
 import { ArticlesList } from '../modules/articles/components/articlesList.component';
+import { CartLayout } from '../modules/cart/components/cartLayout.component';
 
 
 export const HomePage = () => (
   <Layout>
     <h2>Home page</h2>
-    <ArticlesList />
+    <CartLayout>
+      <ArticlesList />
+    </CartLayout>
   </Layout>
 );
