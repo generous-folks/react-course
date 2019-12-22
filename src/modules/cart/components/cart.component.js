c0da1b2c7761b6dc83f3047809178ce8131506e4
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2019-12-22 09:48:09 +0100

    feat(modal): add custom modal component in cart

--- src/modules/cart/components/cart.component.js
+++ src/modules/cart/components/cart.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import PropTypes from 'prop-types'
 
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-
 import { makeStyles } from '@material-ui/core/styles';
+
+import { Modal } from '../../../components/modal.component';
 
 const useStyles = makeStyles({
   card: {
@@ -23,26 +23,33 @@ const useStyles = makeStyles({
 
 export function Cart() {
   const classes = useStyles();
+  const [showModal, setShowModal] = React.useState(false);
+
+  const toggleModal = () => setShowModal(!showModal);
+
 
   return (
-    <Card className={classes.card}>
-      <CardContent className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-          Cart
-        </Typography>
-        <Typography>
-          some stuff
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary" variant="outlined">
-          Check out
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card className={classes.card}>
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Cart
+          </Typography>
+          <Typography>
+            some stuff
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={toggleModal} size="small" color="secondary" variant="outlined">
+            Check out
+          </Button>
+        </CardActions>
+      </Card>
+      {showModal &&
+        <Modal title="Checkout modal" closeModal={() => setShowModal(false)} >
+          <p>checkout</p>
+        </Modal>
+      }
+    </>
   );
-}
-
-Cart.propTypes = {
-
 }
