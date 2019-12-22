2275b401185df8c3fa3ea3d8f6e3e5beb6ebf628
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2019-12-22 10:12:00 +0100

    feat(useSelector): create useSelector generic api

--- src/modules/articles/components/articlesList.component.js
+++ src/modules/articles/components/articlesList.component.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 
 import { ArticleCard } from './articleCard.component';
 
-import { useArticlesSelector } from '../articles.hooks';
+import { useArticlesSelector } from '../articles.selectors';
 
 
 export function ArticlesList() {
