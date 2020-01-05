7728fe014bca39a70a457c83b4f153241b6a3c89
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2020-01-05 23:24:21 +0100

    feat(exo): add exercise 3 (#29)

---
+++ exercise-3/src/pages/home.page.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { getArticles } from '../utils/api.utils';
+
+export const HomePage = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    try {
+      if (articles.length === 0) {
+        getArticles()
+          .then(res => mounted && setArticles(res))
+          .catch(err => console.error(err));
+      }
+    } catch (error) {
+      throw new Error(`Fetch articles failed: ${error.message}`);
+    }
+    return () => (mounted = false);
+  }, [articles]);
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Link to="/about">About Page</Link>
+      <div>
+        <h4>Articles</h4>
+        <ul>{articles.length > 0 && articles.map(({ id, name }) => <li key={id}>{name}</li>)}</ul>
+      </div>
+    </div>
+  );
+};
