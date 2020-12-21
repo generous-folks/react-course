f966c48b9b770d792a8f65ff7342e009012b76cb
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2020-12-21 17:04:17 +0100

    lets end this

--- exercise-8/src/components/__tests__/navbar.component.spec.js
+++ exercise-8/src/components/__tests__/navbar.component.spec.js
@@ -7,7 +7,10 @@ import { AppBar, IconButton, Menu, Typography } from '@material-ui/core';
 import NavBar from '../navbar.component';
 
 let wrapper;
-
+jest.mock('../../modules/user/user.context.js', () => ({
+  useUserState: jest.fn().mockReturnValue({ user: null }),
+  useUser: jest.fn().mockReturnValue([{ user: null }, jest.fn()]),
+}));
 const getWrapper = () => shallow(<NavBar />);
 
 describe('<NavBar />', () => {
@@ -24,14 +27,22 @@ describe('<NavBar />', () => {
   describe('Markup checks', () => {
     it('should contain the correct markup', () => {
       expect(wrapper.find(AppBar).exists()).toBeTruthy();
-      expect(wrapper.find(Typography).text()).toBe('Shopping App');
+      expect(
+        wrapper
+          .find(Typography)
+          .first()
+          .text(),
+      ).toBe('Shopping App');
     });
   });
 
   describe('Interaction checks', () => {
     it('should sur anchorEl from clicked item', () => {
       expect(wrapper.find(Menu).prop('open')).toBeFalsy();
-      wrapper.find(IconButton).prop('onClick')({ currentTarget: 'foo' });
+      wrapper
+        .find(IconButton)
+        .last()
+        .prop('onClick')({ currentTarget: 'foo' });
       wrapper.update();
       expect(wrapper.find(Menu).prop('open')).toBeTruthy();
     });
